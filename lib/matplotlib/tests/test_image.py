14d077e9f0ed85b5c29b9a8081c883634ebab551
hoshaun <shaun.ho@mail.utoronto.ca>
2018-03-15 22:49:43 -0400

    separate bug5 unit test from existing tests

--- lib/matplotlib/tests/test_image.py
+++ lib/matplotlib/tests/test_image.py
@@ -876,11 +876,16 @@ def test_empty_imshow(make_norm):
     with pytest.raises(RuntimeError):
         im.make_image(fig._cachedRenderer)
 
-@image_comparison(baseline_images=['imshow_longdouble'])
 def test_imshow_float128():
     fig, ax = plt.subplots()
     ax.imshow(np.zeros((3, 3), dtype=np.longdouble))
-    plt.show()
+
+@image_comparison(baseline_images=['imshow_longdouble'],
+                  remove_text=True, extensions=['png'])
+def test_imshow_longdouble():
+    fig, ax = plt.subplots()
+    ax.imshow(np.zeros((3, 3), dtype=np.longdouble))
+    plt.show()    
 
 def test_imshow_bool():
     fig, ax = plt.subplots()
